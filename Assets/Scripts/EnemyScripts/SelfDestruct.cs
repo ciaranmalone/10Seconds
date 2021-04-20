51ea44ff9148df4e6771a80c118052dcd09de969
JackTyrrell00 <c00250170@itcarlow.ie>
2021-04-20 22:21:24 +0100

    Again another branch because lazy

--- Assets/Scripts/EnemyScripts/SelfDestruct.cs
+++ Assets/Scripts/EnemyScripts/SelfDestruct.cs
@@ -2,17 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class SelfDestruct : MonoBehaviour
 {
     [SerializeField] private float destructTime = 1f;
+   
     void Start()
     {
         StartCoroutine(SelfDestructCall());
+        //AudioSource playMe = gameObject.AddComponent(typeof(AudioSource)) as AudioSource;
+        //if (explosiveNoise != null)
+        //{
+        //    playMe.clip = explosiveNoise;
 
+        //}
+        //else
+        //{
+        //    playMe.clip = Resources.Load<AudioClip>("Bass boosted Yoda death sound");
+        //}
+        //playMe.PlayOneShot(playMe.clip, 1); //Just play is deprecated
     }
 
     IEnumerator SelfDestructCall()
     {
+
         yield return new WaitForSeconds(destructTime);
         Destroy(gameObject);
     }
