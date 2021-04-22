cbbdd6537a3fb0e6e2bd68595ba3a1a51f5d90d1
Ciarán Malone <ciaranmalone27@gmail.com>
2021-04-22 02:49:59 +0100

    with all the sounds and hard mode started

--- Assets/Scripts/StartGame.cs
+++ Assets/Scripts/StartGame.cs
@@ -7,7 +7,9 @@ public class StartGame : MonoBehaviour
 {
     [SerializeField] PlayersTimeCounter playersTimeCounter;
     [SerializeField] private KeyCode start;
+    [SerializeField] private KeyCode hardMode;
     [SerializeField] private string firstScene;
+    [SerializeField] private string hardModeScene;
 
 
     // Update is called once per frame
@@ -18,6 +20,11 @@ public class StartGame : MonoBehaviour
             playersTimeCounter.StartTimer();
             SceneManager.LoadScene(firstScene);
 
+        }
+
+        if(Input.GetKeyDown(hardMode))
+        {
+            SceneManager.LoadScene(hardModeScene);
         }
     }
 }
